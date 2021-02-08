499527475c87d2a7a5b42be3663fc6a8b921c29d
Álvaro Camacho <mauricio-xxi@hotmail.com>
2021-02-08 23:52:42 +0100

    add project flip card

---
+++ portfolio/src/components/ProjectCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const ProjectCard = ({ title, src, message }) => {
+    return (
+        <div className="flip-card">
+            <div className="flip-card-inner">
+                <div className="flip-card-front">
+                    <img
+                        src={src}
+                        alt="Avatar"
+                        style={{ width: '300px', height: '300px' }}
+                    />
+                </div>
+                <div className="flip-card-back">
+                    <h1>{title}</h1>
+                    <p>Architect & Engineer</p>
+                    <p>{message}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
