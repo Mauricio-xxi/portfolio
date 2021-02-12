e44c14d9c54db996a2780cb19a9c5d6b5c77d320
Álvaro Camacho <mauricio-xxi@hotmail.com>
2021-02-12 23:49:24 +0100

    add header to about, pending to add a reusable component

--- portfolio/src/pages/About.js
+++ portfolio/src/pages/About.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
 export const About = () => {
-    return <div>About</div>
+    return (
+        <>
+            <div className={'App-header'} style={{ minHeight: '30vh' }}>
+                <h1 style={{ color: '#fcee3d' }}>About</h1>
+            </div>
+            <div style={{ minHeight: '70vh' }}></div>
+        </>
+    )
 }
