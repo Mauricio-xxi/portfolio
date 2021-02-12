0bbae6ff53f114cd4aa9ef5c2e21f6f585e29989
Álvaro Camacho <mauricio-xxi@hotmail.com>
2021-02-12 23:49:32 +0100

    add header to blog

--- portfolio/src/pages/Blog.js
+++ portfolio/src/pages/Blog.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
 export const Blog = () => {
-    return <div>Blog</div>
+    return (
+        <>
+            <div className={'App-header'} style={{ minHeight: '30vh' }}>
+                <h1 style={{ color: '#fcee3d' }}>Blog</h1>
+            </div>
+            <div style={{ minHeight: '70vh' }}></div>
+        </>
+    )
 }
