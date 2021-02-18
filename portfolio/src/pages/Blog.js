bafec93e36e39f35716517d302e815dfecf5252e
Álvaro Camacho <mauricio-xxi@hotmail.com>
2021-02-18 23:55:29 +0100

    fix blog header and add a reusable compoent

--- portfolio/src/pages/Blog.js
+++ portfolio/src/pages/Blog.js
@@ -1,11 +1,10 @@
 import React from 'react'
+import { PagesHeader } from '../components/PagesHeader'
 
 export const Blog = () => {
     return (
         <>
-            <div className={'App-header'} style={{ minHeight: '30vh' }}>
-                <h1 style={{ color: '#fcee3d' }}>Blog</h1>
-            </div>
+            <PagesHeader name={'Blog'} />
             <div style={{ minHeight: '70vh' }}></div>
         </>
     )
